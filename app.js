2a75ade9fb04231563578fd52de83a7a8b81cfe0
apiwitwutithumkitti <apiwit.wutithumkitti@outlook.com>
2021-09-19 17:14:46 +0700

    Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = new express();
 
@@ -36,34 +37,39 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password){
-                    res.render("secrets");
-                } else {
-                    res.render("login");
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if (result === true){
+                        res.render("secrets");
+                    } else {
+                        res.render("login");
+                    }
+                });
             } else {
                 res.render("login");
             }
